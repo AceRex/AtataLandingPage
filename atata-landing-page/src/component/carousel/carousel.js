e5f7993ae0ffd8697d617254bb146f8db5851c82
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-12-15 08:01:38 +0100

    PushIt

--- atata-landing-page/src/component/carousel/carousel.js
+++ atata-landing-page/src/component/carousel/carousel.js
@@ -5,28 +5,38 @@ import Img1 from "./model-Image.png";
 import Img2 from "./sculpture.png";
 import Img3 from "./zebra.png";
 import Img4 from "./map.png";
+import Img5 from "./art.png";
+
 
 export default function carousel() {
   return (
     <div className="container bg-carousel">
       <div className="animation-container">
         <TextLoop interval="5000">
-            <span>
-              <img src={Img1} className="model-img" />
-            </span>
-            <span className='text'>Sculpture at your Click</span><span>
-              <img src={Img3} className="zebra-img" />
-            </span>
-            <span className='text'>One Big Africa Art Market</span>
+          <span>
+            <img src={Img1} className="model-img" />
+          </span>
+          <span className="text">Sculpture at your Click</span>
+          <span>
+            <img src={Img3} className="zebra-img" />
+          </span>
+          <span className="text">One Big Africa Art Market</span>
+          <span>
+            <img src={Img5} className="Art-img" />
+          </span>
         </TextLoop>{" "}
         <TextLoop interval="5000">
-            <span className='text text-mobile'><p>African Artworks </p>and Handicrafts Home</span>            
-            <span>
-              <img src={Img2} className="sculp-img" />
-            </span><span className='text'>Animal Skin at ease</span>            
-            <span>
-              <img src={Img4} className="map-img" />
-            </span>
+          <span className="text text-mobile">
+            <p>African Artworks </p>and Handicrafts Home
+          </span>
+          <span>
+            <img src={Img2} className="sculp-img" />
+          </span>
+          <span className="text">Animal Skin at ease</span>
+          <span>
+            <img src={Img4} className="map-img" />
+          </span>
+          <span className="text"><p>Buy and Bid for</p>African Painting</span>
         </TextLoop>{" "}
       </div>
       <div className="image-slider"></div>
